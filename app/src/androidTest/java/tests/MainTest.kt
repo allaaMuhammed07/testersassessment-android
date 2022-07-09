a64d55c0c94190c9a388d1c09aef8aa34f8d7fd8
allaaismail <allaamuhammed.7@gmail.com>
2022-07-09 21:56:04 +0200

    Setting up the framework

---
+++ app/src/androidTest/java/tests/MainTest.kt
@@ -0,0 +1,11 @@
+package tests
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainTest {
+
+}
---
+++ app/src/main/java/screens/AssessmentScreen.kt
@@ -0,0 +1,7 @@
+package screens
+
+import com.abnamro.apps.referenceandroid.R
+
+class AssessmentScreen {
+    private val screenHeader: Int = R.id.toolbar
+}
